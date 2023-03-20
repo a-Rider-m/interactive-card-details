d39eb6acd6d8a6e5dafe3c3d3d28540a6934f884
Miguel Angel Arias <miguel.a.frontend@gmail.com>
2023-03-19 20:04:53 -0500

    Creacion funcion para datos tarjeta

--- JS/index.js
+++ JS/index.js
@@ -6,17 +6,6 @@ const continueButton = document.querySelector('#continue-button');
 const formContainer = document.querySelector('.form-container');
 const successContainer = document.querySelector('.success-container');
 
-//Inputs formulario
-const inputName = document.querySelector('.card-holder-name');
-const cardName = document.querySelector('.card-container__name');
-const inputNumber = document.querySelector('.card-number');
-const cardNumber = document.querySelector('.card-container__number');
-const inputMonth = document.querySelector('.input-month');
-const cardMonth = document.querySelector('.card-container__month');
-const inputYear = document.querySelector('.input-year');
-const cardYear = document.querySelector('.card-container__year');
-const inputCvc = document.querySelector('.input-cvc');
-const cardCvc = document.querySelector('.card-container__segurity-code');
 
 const cleave = new Cleave('.card-number', {
     creditCard: true,
@@ -38,43 +27,21 @@ function refreshSite() {
     document.location.reload();
 }
 
-inputName.addEventListener('input', () => {
-    cardName.innerText = inputName.value;
+function updateCardInfo (inputSelector, outPutSelector, defaulText) {
+    const inputs = document.querySelector(inputSelector);
+    const outPutCard = document.querySelector(outPutSelector);
 
-    if(inputName.value.length === 0) {
-        cardName.innerText = "Jane Appleseed";
-    }
-});
+    inputs.addEventListener('input', () => {
+        outPutCard.innerText = inputs.value;
+    
+        if(inputs.value.length === 0) {
+            outPutCard.innerText = defaulText;
+        }
+    });
+}
 
-inputNumber.addEventListener('input', () => {
-    cardNumber.innerText = inputNumber.value;
-
-    if(inputNumber.value.length === 0) {
-        cardNumber.innerText = "0000 0000 0000 0000";
-    }
-});
-
-inputMonth.addEventListener('input', () => {
-    cardMonth.innerText = inputMonth.value;
-
-    if(inputMonth.value.length === 0) {
-        cardMonth.innerText = "00";
-    }
-});
-
-inputYear.addEventListener('input', () => {
-    cardYear.innerText = inputYear.value;
-
-    if(inputYear.value.length === 0) {
-        cardYear.innerText = "00";
-    }
-});
-
-inputCvc.addEventListener('input', () => {
-    cardCvc.innerText = inputCvc.value;
-
-    if(inputCvc.value.length === 0) {
-        cardCvc.innerText = "000";
-    }
-});
-
+updateCardInfo('.card-holder-name', '.card-container__name', "Jane appleseed");
+updateCardInfo('.card-number', '.card-container__number', "0000 0000 0000 0000");
+updateCardInfo('.input-month', '.card-container__month', "00");
+updateCardInfo('.input-year', '.card-container__year', "00");
+updateCardInfo('.input-cvc', '.card-container__segurity-code', "000");
