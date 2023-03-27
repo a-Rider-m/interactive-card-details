0473dc448783441f610b0677db442bbd65fce456
Miguel Angel Arias <miguel.a.frontend@gmail.com>
2023-03-26 20:18:07 -0500

    Update function names from spanish to english

--- JS/index.js
+++ JS/index.js
@@ -1,4 +1,4 @@
-const formulario = document.querySelector('.form-container');
+const form = document.querySelector('.form-container');
 
 //Botones
 const confirmButton = document.querySelector('.main-button');
@@ -9,37 +9,41 @@ const formContainer = document.querySelector('.form-container');
 const successContainer = document.querySelector('.success-container');
 
 const everyInput = document.querySelectorAll('.form-container__input')
-const expresiones = {
+const expressions = {
     name: /^[A-Za-zÁ-Úá-úüÜñÑ]+(?:\s[A-Za-zÁ-Úá-úüÜñÑ]+)*$/, 
-    number: /^\d(?:\s?\d){15}$/,
+    cardNumber: /^\d(?:\s?\d){15}$/,
     month: /^(0[1-9]|1[0-2])$/,
     year: /^\d{2}$/,
     cvc: /^\d{3}$/
 }
 
+//Formato de tarjeta bancaria
+const cleave = new Cleave('.card-number', {
+    creditCard: true,
+});
 
-//corregir y agregar una clase individual para cada etiquta P de error
-function validarFormulario(e) {
+function validateForm(e) {
     switch(e.target.name) {
         case "card-holder":
-            validarCampo('name-card', "name", expresiones.name, e.target);
+            validateField('name-card', "name", expressions.name, e.target);
         break;
         case "card-number":
-            validarCampo('number-card', "number", expresiones.number, e.target);
+            validateField('number-card', "number", expressions.cardNumber, e.target);
         break;
         case "card-month":
-            validarCampo('date-month', "date", expresiones.month, e.target);
+            validateField('date-month', "date", expressions.month, e.target);
         break;
         case "card-year":
-            validarCampo('date-year', "", expresiones.year, e.target);
+            validateField('date-year', "", expressions.year, e.target);
         break;
         case "card-cvc":
-            validarCampo('segurity-code', "cvc", expresiones.cvc, e.target);
+            validateField('segurity-code', "cvc", expressions.cvc, e.target);
         break;
     }
 };
 
-function validarCampo(idName, className, expresion, input) {
+//Validacion de la informacion en cada uno de los campos
+function validateField(idName, className, expresion, input) {
     if(expresion.test(input.value)) {
         document.getElementById(idName).classList.remove('form-container__error-format');
         document.getElementById(idName).classList.add('form-container__input-correct');
@@ -52,11 +56,13 @@ function validarCampo(idName, className, expresion, input) {
 }
 
 everyInput.forEach((input) => {
-    input.addEventListener('keyup', validarFormulario);
-    input.addEventListener('blur', validarFormulario);
+    input.addEventListener('keyup', validateForm);
+    input.addEventListener('blur', validateForm);
 })
 
-formulario.addEventListener('submit', (e)=> {
+
+//Se verifica que todos los campos sean correctos
+form.addEventListener('submit', (e)=> {
     e.preventDefault();
 
     let formValido = true;
@@ -80,11 +86,6 @@ formulario.addEventListener('submit', (e)=> {
 
 });  
 
-//Formato de tarjeta bancaria
-const cleave = new Cleave('.card-number', {
-    creditCard: true,
-});
-
 //Funcion para actualizar en tiempo real los datos de tarjeta
 function updateCardInfo (inputSelector, outPutSelector, defaulText) {
     const input = document.querySelector(inputSelector);
@@ -105,6 +106,7 @@ updateCardInfo('.input-month', '.card-container__month', "00");
 updateCardInfo('.input-year', '.card-container__year', "00");
 updateCardInfo('.input-cvc', '.card-container__segurity-code', "000");
 
+//Funcion que marca como exitosa la informacion en cada campo
 function exitoFormulario() {
     const isFormInactive = formContainer.classList.contains('inactive');
 
@@ -114,6 +116,7 @@ function exitoFormulario() {
     }
 }
 
+//Boton que reinicia la pagina
 continueButton.addEventListener('click', () => {
     document.location.reload();
 });
